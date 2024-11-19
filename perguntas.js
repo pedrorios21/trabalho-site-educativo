1daa4b45d1ddcffb1be441e42f48cf50474a9da4
josé <romaozinho2006@gmail.com>
2024-11-19 09:55:41 -0300

    first

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa uma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um loop?',
+    'É uma estrutura que repete um código enquanto uma condição for verdadeira'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'É um espaço na memória que armazena um valor'
+)
